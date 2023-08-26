7b8d950d9fbc240bfb037e0487916b62b4c16c60
김우찬 <woochankim0303@gmail.com>
2023-08-26 21:44:57 +0900

    Effect: Key Hit Effect
    반투명 노트 치기 효과 추가

---
+++ Assets/Scripts/WC/Notes/Key.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace onLand
+{
+    public class Key : MonoBehaviour
+    {
+        private Color originColor;
+        private SpriteRenderer spriteRenderer;
+        [SerializeField]
+        private GameObject effectKey;
+        private SpriteRenderer effectSpriteRenderer;
+        
+        void Start()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            originColor = spriteRenderer.color;
+            effectSpriteRenderer = effectKey.GetComponent<SpriteRenderer>();
+            effectKey.SetActive(false);
+            
+        }
+        private void OnCollisionEnter2D(Collision2D other)
+        {   
+            if (other.gameObject.CompareTag("Player"))
+            {
+                GetComponent<SpriteRenderer>().color = Color.red;
+                StartCoroutine(EnterKeyEffect());
+            }
+        }
+        private void OnCollisionExit2D(Collision2D other)
+        {   
+            if (other.gameObject.CompareTag("Player"))
+            {
+                GetComponent<SpriteRenderer>().color = originColor;
+                StartCoroutine(ExitKeyEffect());
+                //effectKey.SetActive(false);
+            }
+        }
+
+        public IEnumerator EnterKeyEffect()
+        {
+            StopCoroutine(ExitKeyEffect());
+            effectKey.SetActive(true);
+            for (float f = 0f; f < 1f; f += 0.1f)
+            {
+                Color c = effectSpriteRenderer.material.color;
+                c.a = f;
+                effectSpriteRenderer.material.color = c;
+                yield return null;
+            }
+        }
+
+        public IEnumerator ExitKeyEffect()
+        {
+            StopCoroutine(EnterKeyEffect());
+            for (float f = 1f; f > 0f; f -= 0.01f)
+            {
+                Color c = effectSpriteRenderer.material.color;
+                c.a = f;
+                effectSpriteRenderer.material.color = c;
+                yield return null;
+            }
+            effectKey.SetActive(false);
+        }
+    }
+}
+
