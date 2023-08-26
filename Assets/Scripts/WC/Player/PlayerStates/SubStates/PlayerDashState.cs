10f4079d9f484d8b9c4dbbc2c051a7822f321263
김우찬 <woochankim0303@gmail.com>
2023-08-26 18:23:46 +0900

    Update PlayerDashState.cs
    불필요한 내용 제거
    - isHolding: 대시 입력 시 방향 정하기 위한 불릿타임 삭제

--- Assets/Scripts/WC/Player/PlayerStates/SubStates/PlayerDashState.cs
+++ Assets/Scripts/WC/Player/PlayerStates/SubStates/PlayerDashState.cs
@@ -7,7 +7,6 @@ namespace onLand
     public class PlayerDashState : PlayerAbilityState
     {
         public bool CanDash { get; private set; }
-        private bool isHolding;
 
         private float lastDashTime;
 
@@ -24,13 +23,13 @@ namespace onLand
             CanDash = false;
             player.InputHandler.UseDashInput();
 
-            isHolding = true;
             dashDirection = Vector2.right * player.FacingDirection;
 
             //slow
             //Time.timeScale = playerData.holdTimeScale;
             
-            startTime = Time.unscaledTime;
+            //startTime = Time.unscaledTime;
+            startTime = Time.time;
             Debug.Log("Dash Enter");
         }
 
@@ -51,39 +50,13 @@ namespace onLand
             
             if (!isExitingState)
             {
-                if (isHolding) // 타임스케일 조정
+                player.SetVelocity(playerData.dashVelocity, dashDirection);
+
+                if(Time.time >= startTime + playerData.dashTime)
                 {
-                    dashDirectionInput = player.InputHandler.DashDirectionInput;
-
-                    if (dashDirectionInput != Vector2.zero)
-                    {
-                        dashDirection = dashDirectionInput;
-                        dashDirection.Normalize();
-                    }
-
-                    float angle = Vector2.SignedAngle(Vector2.right,dashDirection);
-                    player.DashDirectionIndicator.rotation = Quaternion.Euler(0f, 0f, angle-45f);
-
-                    //Dash
-                    if (Time.unscaledTime >= startTime + playerData.maxHoldTime)
-                    {
-                        isHolding = false;
-                        startTime = Time.time;
-                        player.CheckIfShouldFlip(Mathf.RoundToInt(dashDirection.x));
-                        player.RB.drag = playerData.drag;
-                        player.SetVelocity(playerData.dashVelocity, dashDirection);
-                    }
-                }
-                else
-                {
-                    player.SetVelocity(playerData.dashVelocity, dashDirection);
-
-                    if(Time.time >= startTime + playerData.dashTime)
-                    {
-                        player.RB.drag = 0f;
-                        isAbilityDone = true;
-                        lastDashTime = Time.time;
-                    }
+                    player.RB.drag = 0f;
+                    isAbilityDone = true;
+                    lastDashTime = Time.time;
                 }
             }
         }
