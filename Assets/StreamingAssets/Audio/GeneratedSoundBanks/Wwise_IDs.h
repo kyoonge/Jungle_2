4c643f40c162cb583d4d52326e3e0d7f38cbbf91
MeganTrioMusic <coomott@naver.com>
2023-08-27 21:14:31 +0900

    중간 저장

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -15,8 +15,9 @@ namespace AK
     {
         static const AkUniqueID COUNT = 2938671170U;
         static const AkUniqueID GAMEOVER = 4158285989U;
-        static const AkUniqueID KEYSOUND = 2641283287U;
+        static const AkUniqueID ITEMSOUND = 2978212717U;
         static const AkUniqueID MAINSONG = 3863200005U;
+        static const AkUniqueID NOTESOUND = 3937133458U;
         static const AkUniqueID TESTSONG = 118783190U;
     } // namespace EVENTS
 
